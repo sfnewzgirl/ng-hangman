49ba4be9a79515ae7c41f85e669fcbf581034715
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-09-14 16:40:14 -0700

    finished hangman training

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -2,3 +2,13 @@ console.log('app.js loaded!');
 
 // initialize the application
 var app = angular.module("hangmanApp", []);
+app.controller("hangmanCtrl", hangmanCtrl);
+
+function hangmanCtrl() {
+  var vm = this;
+  vm.game = new HangmanGame("elephant");
+  vm.guessLetter = function () {
+    vm.game.guess(vm.input);
+    vm.input = "";
+  }
+}
